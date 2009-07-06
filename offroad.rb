4152c50dec0de3ffeca945d00c3ed5962fd53f4d
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-06 19:51:52 +0200

    Use relative load.

--- offroad.rb
+++ offroad.rb
@@ -1,5 +1,12 @@
-load 'reset.rb'
-load 'ext.rb'
+require 'capistrano'
+
+def _load_relative(file)
+  load(File.join(File.dirname(__FILE__), file))
+end
+
+_load_relative 'reset.rb'
+_load_relative 'ext.rb'
+
 def offroad_modules(*modules)
-  modules.each { |mod| load("modules/#{mod}.rb"); }
+  modules.each { |mod| _load_relative "modules/#{mod}.rb" }
 end
