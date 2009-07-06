41ac070fe5cefadb9e709637b98fbfb399d2d521
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-06 22:43:00 +0200

    Correctly restart daemontools on deploy.

--- modules/daemontools.rb
+++ modules/daemontools.rb
@@ -1,9 +1,23 @@
 # Running via demontools
 
 set :svscan_root, "/service"
+set :supervise_name, "#{application}"
 
 def svc(cmd)
-  sudo "svc #{cmd} #{svscan_root}/#{application}"
+  sudo "svc #{cmd} #{svscan_root}/#{supervise_name}"
+end
+
+namespace :daemontools do
+  task :create_symlink do
+    sudo "ln -s -v #{current_path} #{svscan_root}/#{supervise_name}"
+  end
+  task :remove_symlink do
+    sudo "rm -v #{svscan_root}/#{supervise_name}"
+  end
+  task :remove do
+    remove_symlnk
+    sudo "svc -x #{current_path}"
+  end
 end
 
 namespace :deploy do
@@ -15,5 +29,12 @@ namespace :deploy do
   end
   task :restart do
     svc "-t"
+  end
+  task :symlink do
+    on_rollback { run "rm -f #{current_path}; ln -s #{previous_release} #{current_path}; true" } # FIXME!
+    run "rm -f #{current_path}"
+    sleep 5
+    sudo "svc -t -x #{previous_release}"
+    run "ln -s #{latest_release} #{current_path}"
   end
 end
