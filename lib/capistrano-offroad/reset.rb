f9f5ec2997fc9d6a8430b7165a03fda184547af3
Maciej Pasternacki <maciej@pasternacki.net>
2010-05-28 21:33:49 +0200

    You can supply :no_chgrp or :sudo_chgrp as value for :group_writable - fixes #1

--- lib/capistrano-offroad/reset.rb
+++ lib/capistrano-offroad/reset.rb
@@ -8,7 +8,13 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     task :finalize_update, :except => { :no_release => true } do
       if fetch(:group_writable, true)
-        sudo "chgrp -R #{deploy_group} #{latest_release}"
+        if :no_chgrp == fetch(:group_writable, true)
+          # skip step
+        elsif :sudo_chgrp == fetch(:group_writable, true)
+          sudo "chgrp -R #{deploy_group} #{latest_release}"
+        else
+          run "chgrp -R #{deploy_group} #{latest_release}"
+        end
         run "chmod -R g+w #{latest_release}"
       end
     end
