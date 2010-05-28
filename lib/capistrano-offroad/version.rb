c8c3cccff7266895e0a00eaefbb822e03de52540
Maciej Pasternacki <maciej@pasternacki.net>
2010-05-28 21:42:35 +0200

    Bump version to 0.1.0.

--- lib/capistrano-offroad/version.rb
+++ lib/capistrano-offroad/version.rb
@@ -1,8 +1,8 @@
 module CapistranoOffroad
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY  = 1
+    MINOR = 1
+    TINY  = 0
     STRING = [MAJOR, MINOR, TINY].join('.')
 
     def VERSION.require_version(major, minor=0, tiny=0)
